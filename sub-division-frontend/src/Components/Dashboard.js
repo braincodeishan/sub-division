496ec494a764991418f690744934eb4367cbf5cd
braincodeishan <braincodeishan@gmail.com>
2022-01-21 16:55:09 +0530

    blob edit in dashboard

--- sub-division-frontend/src/Components/Dashboard.js
+++ sub-division-frontend/src/Components/Dashboard.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Blobs from './sub-component/Blobs'
 const Dashboard = () => {
     return (
         <>
-            <section class="fdjkf">
+            <section class="fdjkf col-12">
                 
 
 
-                <div className="card">
+                <div className="card col-12 col-md-12 col-lg-6">
                     <h5 className="card-header">Inspection Details</h5>
                     <div className="card-body">
                         <h5 className="card-title">Targets</h5>
-                        <p className="card-text">Brief description of Targets given, and achieved</p>
+                        <p className="card-text">Inspections is to be done this year is <b>X</b>.</p><p>The total inspections as per the tour program till today is <b>X</b>. </p><p>The total inspection pending till today is <b>X</b>. </p><p>IR to be issued is <b>X</b> and IR to be prepared is <b>X</b>.</p>
 
                         {/* <Link to="#" className="btn btn-primary">View Summary</Link> */}
                     </div>
                 </div>
+                {<Blobs/>}
 
             </section>
         </>
