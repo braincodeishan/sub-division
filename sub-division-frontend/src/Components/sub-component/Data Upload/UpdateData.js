1efbca679b919e17755726124b79de1087272a02
Ishan Dev <braincodeishan@gmail.com>
2022-11-13 01:21:32 +0530

    13.12.2022

--- sub-division-frontend/src/Components/sub-component/Data Upload/UpdateData.js
+++ sub-division-frontend/src/Components/sub-component/Data Upload/UpdateData.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const UpdateData = () => {
   return (
-    <div className='container'>
-        <div></div>
+    <div className='container-fluid'>
+        <div className='spacer5 offWhite'></div>
+          <p className=''>Please select the type of data to be uploaded</p>
+
+        <div className='spacer5 offWhite'></div>
     </div>
   )
 }
