9e55f2893434fea59f36614cecc6196bbae19231
Ishan Dev <braincodeishan@gmail.com>
2023-03-18 12:23:48 +0530

    final build

--- frontend/src/Components/sub-component/ChargeReport/ChargeReport.js
+++ frontend/src/Components/sub-component/ChargeReport/ChargeReport.js
@@ -4,6 +4,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import ShowChargeReport from './ShowChargeReport';
 
+
 const ChargeReport = () => {
     
     const [showData, setShowData] = useState(true);
