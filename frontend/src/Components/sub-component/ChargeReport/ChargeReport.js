893e0d9ee8e4219633297916d0400b07fae6ae46
Ishan Dev <braincodeishan@gmail.com>
2023-03-18 12:28:42 +0530

    final

--- frontend/src/Components/sub-component/ChargeReport/ChargeReport.js
+++ frontend/src/Components/sub-component/ChargeReport/ChargeReport.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import ShowChargeReport from './ShowChargeReport';
+import ShowChargeReport from '../ChargeReport/ShowChargeReport';
 
 
 const ChargeReport = () => {
