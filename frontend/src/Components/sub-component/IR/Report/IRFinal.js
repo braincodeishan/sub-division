09f20f184836004a495e12c66afb38ce5361c436
Ishan Dev <braincodeishan@gmail.com>
2023-04-18 23:55:07 +0530

    minor change

--- frontend/src/Components/sub-component/IR/Report/IRFinal.js
+++ frontend/src/Components/sub-component/IR/Report/IRFinal.js
@@ -686,8 +686,8 @@ const IRFinal = ({ pageS1, pageS2, pageS3, pageS4, pageS5, pageS6, pageS6DTR, sb
 
                     })}
                     <tr>
-                        <th colSpan={'3'}>TD</th>
                         <th colSpan={'3'}>SSA</th>
+                        <th colSpan={'3'}>TD</th>
 
                     </tr>
                     <tr>
